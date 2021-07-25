72a61e7f71169baa5d80ee61eaf35895145360e6
Anatoliy Iliev <vekok12@gmail.com>
2021-07-25 09:59:44 +0300

    day-5

--- src/components/FeedbackOptions/FeedbackOptions.js
+++ src/components/FeedbackOptions/FeedbackOptions.js
@@ -1,12 +1,12 @@
 // import PropTypes from 'prop-types';
-import styles from '../FeedbackOptions/FeedbackOptions.module.scss'
+// import styles from '../FeedbackOptions/FeedbackOptions.module.scss'
 import shortid from 'shortid';
 
 const FeedbackOptions = ({ options, onLeaveFeedback }) => (
     <div>
         {options.map((option) => (
             <button
-            className={styles.button}
+            // className={styles.button}
             type="button"
             key={shortid.generate()}
             onClick={() => onLeaveFeedback(option)}> {option}</button>
